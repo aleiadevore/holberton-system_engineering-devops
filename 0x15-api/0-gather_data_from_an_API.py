9605ff91ad0994beea579a1d6b9e65ff6fb31581
Aleia DeVore <2496@holbertonschool.com>
2021-05-05 19:26:02 +0000

    Fetched from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Returns info about employee todo status """
+
+import requests
+from sys import argv
+
+url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+info = requests.get(url).json()
+name = info.get('name')
+
+params = {'completed': 'true'}
+completed = requests.get('{}/todos'.format(url), params=params).json()
+all_tasks = requests.get('{}/todos'.format(url)).json()
+
+print('Employee {} is done with tasks({}/{}):'.format(
+        name, len(completed), len(all_tasks)))
+for item in completed:
+        print('\t {}'.format(item.get('title')))
