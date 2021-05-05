5e4d851078a0aea14b91998250349da495d9bd0e
Aleia DeVore <2496@holbertonschool.com>
2021-05-05 20:42:48 +0000

    Exports to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Returns info about employee todo status """
+
+
+if __name__ == "__main__":
+        import csv
+        import requests
+        from sys import argv
+
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+        info = requests.get(url).json()
+        name = info.get('name')
+
+        all_tasks = requests.get('{}/todos'.format(url)).json()
+
+        file_name = '{}.csv'.format(argv[1])
+
+        with open(file_name, mode='w') as f:
+                csv_writer = csv.writer(f)
+                for task in all_tasks:
+                        csv_writer.writerow([argv[1], name, task.get(
+                                'completed'), task.get('title')])
