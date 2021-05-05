0eea0dfbd4cf60a668399e239e7c6f57e044cf11
Aleia DeVore <2496@holbertonschool.com>
2021-05-05 19:28:07 +0000

    Doesn't execute on import

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """ Returns info about employee todo status """
 
-import requests
-from sys import argv
 
-url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-info = requests.get(url).json()
-name = info.get('name')
+if __name__ == "__main__":
+        import requests
+        from sys import argv
 
-params = {'completed': 'true'}
-completed = requests.get('{}/todos'.format(url), params=params).json()
-all_tasks = requests.get('{}/todos'.format(url)).json()
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+        info = requests.get(url).json()
+        name = info.get('name')
 
-print('Employee {} is done with tasks({}/{}):'.format(
-        name, len(completed), len(all_tasks)))
-for item in completed:
-        print('\t {}'.format(item.get('title')))
+        params = {'completed': 'true'}
+        completed = requests.get('{}/todos'.format(url), params=params).json()
+        all_tasks = requests.get('{}/todos'.format(url)).json()
+
+        print('Employee {} is done with tasks({}/{}):'.format(
+                name, len(completed), len(all_tasks)))
+        for item in completed:
+                print('\t {}'.format(item.get('title')))
